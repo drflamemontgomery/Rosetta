dae79a92f46568d144e42c0e6c43dd9f8f65528a
drflamemontgomery <drflamemontgomery@gmail.com>
2022-12-19 09:04:46 +1300

    Added header for my custom configs

---
+++ src/configs.h
@@ -0,0 +1,14 @@
+#ifndef ROSSETTA_CONFIGS
+#define ROSSETTA_CONFIGS
+
+#include "hid_dev.h"
+
+extern config_t left_half;
+extern config_t right_half;
+
+extern config_t left_risk_of_rain;
+extern config_t right_risk_of_rain;
+
+void init_configs();
+
+#endif
